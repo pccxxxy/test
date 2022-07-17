e3af055a5819e0c9e8c806e8aa8fd21afc625339
Cindy Tao <77183391+pccxxxy@users.noreply.github.com>
2022-07-17 17:53:36 +1000

    commit week2

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ console.log("This is my first log")
 
 // shift option lower arrow   for copt paste below
 // shift option upper arrow   for copt paste above
+
+let a = 1
+let b = 2
+console.log(a + b)
